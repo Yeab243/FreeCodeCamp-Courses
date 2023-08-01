08f8041883dc64e4f310f4598499e5574b76490e
Yeabsira <103306411+Yeab243@users.noreply.github.com>
2023-08-01 15:06:52 -0700

    Update 2) Roman Numeral Converter.js

--- 2) JavaScript Algorithms and Data Structures/2) Roman Numeral Converter.js
+++ 2) JavaScript Algorithms and Data Structures/2) Roman Numeral Converter.js
@@ -1,1 +1,29 @@
+function convertToRoman(num) {
 
+  let romNumeral = {
+    M:	1000,
+    CM:	900,
+    D:	500,
+    CD:	400,
+    C:	100,
+    XC:	90,
+    L:	50,
+    XL:	40,
+    X:	10,
+    IX:	9,
+    V:	5,
+    IV:	4,
+    I:	1,
+  }
+ let solution = "";
+  for (let items in romNumeral){
+    while(num >= romNumeral[items]){
+      solution += items;
+
+      num -= romNumeral[items];
+    }
+  }
+ return solution;
+}
+
+console.log(convertToRoman(1000));
