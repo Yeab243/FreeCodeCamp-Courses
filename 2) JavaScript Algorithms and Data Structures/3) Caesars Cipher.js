094b71e7ec86febc182216001bc55939b0151f6a
Yeabsira <103306411+Yeab243@users.noreply.github.com>
2023-08-01 15:08:00 -0700

    Update 3) Caesars Cipher.js

--- 2) JavaScript Algorithms and Data Structures/3) Caesars Cipher.js
+++ 2) JavaScript Algorithms and Data Structures/3) Caesars Cipher.js
@@ -1,1 +1,23 @@
+function rot13(str) {
+  let letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  let final = "";
 
+  for (let i = 0; i < str.length; i++) {
+    let char = str[i];
+    let index;
+
+    if (/[A-Z]/.test(char)) {
+      index = (letters.indexOf(char) + 13) % 26;
+      final += letters[index];
+    } else if (/[a-z]/.test(char)) {
+      index = (letters.indexOf(char.toUpperCase()) + 13) % 26;
+      final += letters[index].toLowerCase();
+    } else {
+      final += char;
+    }
+  }
+
+  return final;
+}
+
+console.log(rot13("SERR PBQR PNZC"));
