e72744c8bed770acb7d058a606600af73ecf4999
Yeabsira <103306411+Yeab243@users.noreply.github.com>
2023-08-01 15:04:33 -0700

    Update 1) Palindrome Checker.js

--- 2) JavaScript Algorithms and Data Structures/1) Palindrome Checker.js
+++ 2) JavaScript Algorithms and Data Structures/1) Palindrome Checker.js
@@ -1,1 +1,15 @@
+function palindrome(str) {
+  str = str.replace(/\W|_/g, "").toLowerCase();
 
+  let reverse = str.split("").reverse().join("");
+  if (str=== reverse){
+    return true
+  } else {
+    return false
+  }
+  
+  
+  return true;
+}
+
+console.log(palindrome("eye"));
